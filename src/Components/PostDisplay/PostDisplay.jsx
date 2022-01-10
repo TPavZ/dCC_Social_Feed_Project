c78fb0049a6044aebf23a89fe743df47664f524b
TPavZ <tmpavlic@gmail.com>
2022-01-10 11:51:04 -0600

    Corrected "key error."

--- src/Components/PostDisplay/PostDisplay.jsx
+++ src/Components/PostDisplay/PostDisplay.jsx
@@ -3,9 +3,9 @@ import LikeDislikeButton from "../LikeDislikeButton/LikeDislikeButton";
 const PostDisplay = (props) => {
 
     return (
-        props.parentPosts.map((entry) => {
+        props.parentPosts.map((entry, index) => {
             return (
-                <div>
+                <div key={index}>
                     <h4><strong>{entry.name}</strong> Said:</h4>
                     <div>
                         <p>{entry.post}</p>
